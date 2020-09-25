d408f7896ff92f4c664cbf4a1f7f127470c5d581
jae hun huh <koni114@naver.com>
2020-09-25 14:56:58 +0900

    Update caret.R

--- caret.R
+++ caret.R
@@ -2,32 +2,52 @@
 ## caret package ##
 ###################
 
-
-
-
 ########################
 # 2. 모델 옵션 setting #
 ########################
+# caret::trainControl function
+# 해당 함수에서 설정할 수 있는 항목은 다음과 같음 
+#- 샘플링 종류        --> none, boost632, cv, repeatedcv, LOOCV
+#- 파라미터 튜닝 방법 --> random, grid(none인 경우는 튜닝 안함)
 
-# caret::trainControl 
-# caret::trainControl 
+## Performance Metrics
+#- 어떻게 모델을 요약할 것인지 선택
+# summaryFunction = <R function>
+# classProbs      = <logical>
+# 개인이 만든 함수를 적용할수도 있고, caret에서 제공하는 default summary function 사용 가능
+# - defaultSummary (accuracy, RMSE, etc)
+# - twoClassSummary(for ROC curve)
+# - prSummary(information retrieval)
+#  --> 해당 두 함수는 classProbs = True로 설정
+
+# Subsampling
+# imblanced data 인 경우, subsampling 옵션으로 적용 가능
+# - up, down, smote, rose 
+
 caret::trainControl(
   # 데이터 샘플링 종류 선택 가능
   # none       : 데이터 샘플링 안함  # boot       : 부트스트래핑
   # boot632    : 부트스트래핑 개선된 버전
   # cv         : 교차 검증
   # repeatedcv : 교차 검증의 반복
-  # LOOCV      : Leave One Out  Cross validation(한개의 데이터로만 테스트하고, 나머지 데이터를 훈련 데이터로 하는 방법)
+  # LOOCV      : Leave One Out Cross validation(한개의 데이터로만 테스트하고, 나머지 데이터를 훈련 데이터로 하는 방법)
+  # LGOCV      : Leave group out(number, p option)
+  # LOO        : Leave one out  
+  # oob        : out-of-bags
+  # timeslice  : time-series data 
   method = 'boot',
   # random, grid 방식 중 하나 선택. method = 'none' 경우는 제외
   search,
   # 교차 검증을 몇 겹으로 할 것인가? 부트스트래핑을 몇 번 수행 할 것인가?
   # ex) k-fold에서 k의 수
   number, 
-  repeats, # 데이터 샘플링 반복 횟수
+  repeats,     # 데이터 샘플링 반복 횟수
   verboseIter, #  training log를 print
   classProbs,  #  이진 분류 시, ROC 넓이 값 등 계산 필요시 True 지정
-  p = 0.75     #  LGOCV에서 training Dataset의 percentag를 말함
+  p = 0.75,     #  LGOCV에서 training Dataset의 percentag를 말함
+  summaryFunction, # defaultSummary, twoClassSummary, prSummary
+  classProbs,      # twoClassSummary, prSummary 사용할 경우, TRUE 를 설정
+  sampling,        # 
 )
 
 
@@ -42,6 +62,43 @@ expand.grid(
 # 3. 모델 훈련 #
 ################
 # caret::train function
+# 해당 함수에서 설정할 수 있는 사항은 다음과 같음
+#- method     : 모델 알고리즘
+#- preProcess : 전처리 방식
+#- weights    : 데이터의 가중치
+#- metric     : 모델 평가 방식
+#- trControl  : trainControl function return 객체
+#- tuneGrid   : Grid search, 샘플링 작업 하지 않을 때 사용
+#- tuneLength : random search 횟수
+
+# train 함수에서 모델을 생성할 수 있는 방식은 크게 3가지 
+# - formula 방식
+# - x/y 방식
+# - object 방식
+
+# -  train formula 방식 사용시 항상 dummy variable 생성
+# -  x/y train 방식은 dummary variable을 생성하지 않습니다.(단 모델 자체에서 dummay variable을 만들어 낼 수도 있습니다.)
+
+# 기억하세요!
+#- data에는 column name이 존재해 함
+#- 분류 결과를 위해서는 factor 형을 사용해야 함 
+#- class 수준에 유효한 R name이 있어야 함
+#- train 함수 호출 전 set.seed를 지정시 resample이 고정돼서 사용
+#- 결측값도 같이 처리하고 싶으면, na.option = na.pass를 사용하면 됩니다. 
+#- 일반적으로 test Data에도 NA가 포함되어 있을 경우에 사용함
+#- caret에서 tuning parameter에서 지원하지 않은 hyper parameter 값을 지정하는 경우, train 함수에 parameter로 지정하면 됨
+
+# preprocess
+# preprocess 옵션 값에 전처리 방법을 지정하면 모델 훈련시 자동으로 전처리를 수행해줍니다.
+# - 정규화 옵션       : center, scale, range(0과 1사이로 변환)
+# - 데이터 변환 옵션  : BoxCox, YeoJohnson, expoTrans 
+# - 결측값 대체       : knnImpute, bagImpute(baggingTree 방식을 이용하여 결측값을 tree 모델로 예측하여 처리), medianImpute
+# - 필터링            : corr, nzv, zv
+# - 차원 축소, 그룹변환 : pca, ica(독립 성분 분석), spatialSign
+# 전처리 작동 순서는 입력 순서와는 상관없이 train function 내부적으로 작동 순서를 결정 함
+# recipe package에서 더 다양한 list 확인 가능 
+
+
 caret::train(
   form,   # 모델 포뮬러
   data,   # 분석 데이터
@@ -108,12 +165,12 @@ plot2 <- data.frame('Y'     = Y,
 plot2 <- reshape2::melt(plot2[c(1,2,4)], id.var = 'index')
 names(plot2)[2] <- '구분'
 ggplot(data = plot2, aes(  x = index
-                         , y = value 
-                         , colour = 구분)) + geom_line(linetype = 1
-                                                     , size = 0.5
-                                                     , alpha = 0.6) + scale_colour_manual(values = c('blue', 'red')) + labs(title = 'title',
-                                                                                                                            x     = 'xlab',
-                                                                                                                            y     = 'ylab')
+                           , y = value 
+                           , colour = 구분)) + geom_line(linetype = 1
+                                                       , size = 0.5
+                                                       , alpha = 0.6) + scale_colour_manual(values = c('blue', 'red')) + labs(title = 'title',
+                                                                                                                              x     = 'xlab',
+                                                                                                                              y     = 'ylab')
 
 # 변수 중요도 PLOT
 varImp.data <- caret::varImp(modelResult, scale = T)
@@ -122,13 +179,13 @@ var         <- c('Overall')
 title       <- c('randomForest 예측 변수 중요도')
 
 p <- ggplot(df.plot, aes(x = reorder(rownames(df.plot), - df.plot[, var]),
-                    y = round(df.plot[, var], 3), group = 1))
+                         y = round(df.plot[, var], 3), group = 1))
 
 p <- p + geom_point(size = 2, colour = 'blue') + geom_line(linetype = 2, size = 0.2) + guides(fill = F)
 p <- p + ggtitle(title) + theme(plot.title = element_text(hjust = 0.5))
 p <- p + theme(  axis.text.x  = element_text(size = 14, angle = 30, vjust = 1, hjust = 1)
-               , axis.title.y = element_text(size = 14, angle = 30, vjust = 1, hjust = 1)
-               , panel.grid.major = element_line(colour = "darkgrey", linetype = 'dashed', size = 0.3))
+                 , axis.title.y = element_text(size = 14, angle = 30, vjust = 1, hjust = 1)
+                 , panel.grid.major = element_line(colour = "darkgrey", linetype = 'dashed', size = 0.3))
 p <- p + xlab("Variables") + ylab("Importance")
 print(p)
 
@@ -142,11 +199,11 @@ train.idx  <- caret::createDataPartition(iris[,4], p = 0.8)[[1]]
 data.train <- iris[train.idx , ]
 data.test  <- iris[-train.idx, ]
 modelResult <- train(Petal.Width ~ Sepal.Length + Sepal.Width + Petal.Length
-           , data        = data.train
-           , preProcess  = NULL
-           , method      = 'rf'
-           , ntree       = 1000
-           , trControl   = trainControl(method = 'cv', number = 10, repeats = 3))
+                     , data        = data.train
+                     , preProcess  = NULL
+                     , method      = 'rf'
+                     , ntree       = 1000
+                     , trControl   = trainControl(method = 'cv', number = 10, repeats = 3))
 
 # 결과 비교
 modelResult$results  # 튜닝별 모델링 결과
@@ -220,5 +277,3 @@ bootmean <- foreach(i=1:10000, .combine= c) %do% {
   return(mean(x[sample(1:NROW(x), replace = T)]))
 }
 bootmean[c(10000 *0.025, 10000 * 0.975)]
-
-
